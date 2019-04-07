0f3edb63ed3ac25cfd8d1052197f6522a7521683
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2019-04-07 23:16:50 +0100

    Fonts added. Styling.

--- team_picker/src/components/App/App.js
+++ team_picker/src/components/App/App.js
@@ -11,7 +11,6 @@ import PlayerEntry from '../PlayerEntry/';
 import TeamDisplay from '../TeamDisplay/';
 import WelcomeScreen from '../WelcomeScreen';
 import FourOhFour from '../FourOhFour';
-import LeagueSetup from '../LeagueSetup';
 import LeagueScreen from '../LeagueScreen';
 import Footer from '../Footer';
 
@@ -23,21 +22,21 @@ import '../../styles/style.css';
 class App extends Component {
   render() {
 
-		return (
-			<Router>
-				<div className="app">
-					<Header />
-					<Switch>
-						<Route exact path="/" component={ WelcomeScreen } />
-						<Route exact path="/enter-players" component={ PlayerEntry } />
-						<Route exact path="/display-teams" component={ TeamDisplay } />
-						<Route exact path="/league-dashboard" component={ LeagueScreen } />
-						<Route component={ FourOhFour } />
-					</Switch>
-					<Footer />
-				</div>
-				</Router>
-		);
+	return (
+		<Router>
+			<div className="app">
+				<Header />
+				<Switch>
+					<Route exact path="/" component={ WelcomeScreen } />
+					<Route exact path="/enter-players" component={ PlayerEntry } />
+					<Route exact path="/display-teams" component={ TeamDisplay } />
+					<Route exact path="/league-dashboard" component={ LeagueScreen } />
+					<Route component={ FourOhFour } />
+				</Switch>
+				<Footer />
+			</div>
+		</Router>
+	);
   }
 }
 
